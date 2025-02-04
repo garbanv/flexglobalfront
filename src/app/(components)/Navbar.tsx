b0da601dde25f8ec4b0be4e094ba4e550ff0862c
Alexei Garban <garban.valdeon@gmail.com>
2025-02-04 21:21:14 +0100

    logo udate

--- src/app/(components)/Navbar.tsx
+++ src/app/(components)/Navbar.tsx
@@ -111,8 +111,8 @@ const Nav = () => {
         </ul>
     </div>
       </div>
-      <div className='py-5 flex justify-center border border-t border-b'>
-        <img src="/flexGlobalLogoVertical.svg" alt="flexGlobalLogoVertical" width={300} height={300}/>
+      <div className=' flex justify-center border border-t border-b'>
+        <img src="/flexGlobalLogoHorizontal.svg" alt="flexGlobalLogoVertical" width={300} height={300}/>
       </div>
         {/* <nav className="bg-white  dark:bg-gray-900 fixed w-full z-20 top-0  "> */}
       <div className="max-w-screen-xl  flex flex-wrap items-center justify-between mx-auto p-4">
@@ -156,6 +156,7 @@ const Nav = () => {
           id="navbar-sticky"
         >
           <ul className="flex flex-col w-full p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+           
             <li>
               <a
                 href="#"
